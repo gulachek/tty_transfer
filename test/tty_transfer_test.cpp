3cbe394c099c3f476cecd8cddce29ffef02c0819
Nicholas Gulachek <nsgulachek@gmail.com>
2025-07-08 13:31:09 -0500

    Specify case insensitivity

--- test/tty_transfer_test.cpp
+++ test/tty_transfer_test.cpp
@@ -13,6 +13,7 @@
 
 // https://www.rfc-editor.org/rfc/rfc9562.html
 #define UUID_KEY "68338148-030e-436c-89eb-9f905860f83b"
+#define UUID_KEY_UPPER "68338148-030E-436C-89EB-9F905860F83B"
 #define UUID_KEY2 "11111111-2222-3333-4444-555555555555"
 #define UUID_VAL "f81d4fae-7dec-11d0-a765-00a0c91e6bf6"
 
@@ -41,6 +42,44 @@ TEST(TtyTransferParser, ParsesTokenWithSpacesInBetween) {
       "\e] 1337\t\t;\n\nIOToken  =\v\t" UUID_KEY "  ;\n" UUID_VAL "    \e\\"
       "bar"
       "\e[2;1R";
+
+  tty_transfer_parser *p = tty_transfer_parser_alloc();
+
+  int done = tty_transfer_parser_feed(p, input, std::strlen(input));
+
+  EXPECT_TRUE(done);
+
+  std::string tok = tty_transfer_parser_token_for_key(p, UUID_KEY);
+
+  EXPECT_EQ(tok, UUID_VAL);
+
+  tty_transfer_parser_free(p);
+}
+
+TEST(TtyTransferParser, MatchesIOTokenCaseInsensitively) {
+  const char *input = "foo"
+                      "\e]1337;iotOKEN=" UUID_KEY ";" UUID_VAL "\e\\"
+                      "bar"
+                      "\e[2;1R";
+
+  tty_transfer_parser *p = tty_transfer_parser_alloc();
+
+  int done = tty_transfer_parser_feed(p, input, std::strlen(input));
+
+  EXPECT_TRUE(done);
+
+  std::string tok = tty_transfer_parser_token_for_key(p, UUID_KEY);
+
+  EXPECT_EQ(tok, UUID_VAL);
+
+  tty_transfer_parser_free(p);
+}
+
+TEST(TtyTransferParser, MatchesUUIDCaseInsensitively) {
+  const char *input = "foo"
+                      "\e]1337;IOToken=" UUID_KEY_UPPER ";" UUID_VAL "\e\\"
+                      "bar"
+                      "\e[2;1R";
 
   tty_transfer_parser *p = tty_transfer_parser_alloc();
 
