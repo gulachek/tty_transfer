70fbbc8cdadec063aba7a478bc3bf6d133be9ff1
Nicholas Gulachek <nsgulachek@gmail.com>
2025-07-09 20:03:54 -0500

    Fix tty transfer parser

--- test/release/test.c
+++ test/release/test.c
@@ -2,7 +2,8 @@
 #include <tty_transfer.h>
 
 int main() {
-  tty_transfer_parser p;
-  tty_transfer_parser_reset(&p);
+  tty_transfer_parser *p = tty_transfer_parser_alloc();
+  tty_transfer_parser_reset(p);
+  tty_transfer_parser_free(p);
   return 0;
 }
